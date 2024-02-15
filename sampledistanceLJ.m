3491ce9ea58b47ec5e7b993455f58e3abe20448c
yonicon <159028217+yonicon@users.noreply.github.com>
2024-02-15 18:55:29 +0200

    Update files. for new repo

---
+++ sampledistanceLJ.m
@@ -0,0 +1,10 @@
+%% Simulation of Lennard-Jones potential and resulting force values
+% 
+function [Potential,Force] = SampleDistanceLJ(distance)
+% returns [Joules,N] 
+JoulePerEv=1.60218e-19;
+welldepth=5*JoulePerEv;
+zeropotentialdistance=0.5e-9;
+minimumpotentialdistance=zeropotentialdistance*2^(1/6);
+Potential=4*welldepth*((zeropotentialdistance/distance)^12-(zeropotentialdistance/distance)^6);
+Force=48*welldepth*(((zeropotentialdistance^12)/(distance^13))-(0.5*(zeropotentialdistance^6)/(distance^7)));
