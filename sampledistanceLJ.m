e384aae2ed1caa97840dc8771c8f956312caf7b9
yonicon <159028217+yonicon@users.noreply.github.com>
2024-02-15 19:39:39 +0200

    Fixed switching between potential and force bug.

--- sampledistanceLJ.m
+++ sampledistanceLJ.m
@@ -4,7 +4,7 @@ function [Potential,Force] = SampleDistanceLJ(distance)
 % returns [Joules,N] 
 JoulePerEv=1.60218e-19;
 welldepth=5*JoulePerEv;
-zeropotentialdistance=0.5e-9;
+zeropotentialdistance=0.5e-9; %0.5nm
 minimumpotentialdistance=zeropotentialdistance*2^(1/6);
 Potential=4*welldepth*((zeropotentialdistance/distance)^12-(zeropotentialdistance/distance)^6);
 Force=48*welldepth*(((zeropotentialdistance^12)/(distance^13))-(0.5*(zeropotentialdistance^6)/(distance^7)));
